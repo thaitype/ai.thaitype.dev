10d89bb9e9dc1fcaa37cf6db71af84ae27c067d0
Thada Wangthammang <thada.wth@gmail.com>
2025-05-14 21:13:33 +0700

    migrated to monorepo

---
+++ apps/nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+// import { fetchUsers } from "../data";
+
+export default async function IndexPage() {
+  // const users = await fetchUsers();
+
+  return (
+    <div>
+      <h1>User List: </h1>Í{/* <pre>{JSON.stringify(users, null, 2)}</pre> */}
+    </div>
+  );
+}
